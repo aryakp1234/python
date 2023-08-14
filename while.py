108d0cdbda1fc4449b1413e7f0b00cbd083d6030
arya <aryakp841@gmail.com>
2023-08-14 09:12:14 +0530

    edited

--- while.py
+++ while.py
@@ -1,30 +1,49 @@
+# sum of odd and sum of even
+# ---------------------------
+
+# sumeven=0
+# sumodd=0
 # a=int(input("enter a number :"))
 # i=0
-# sumo=0
-# sume=0
 # while i<=a:
-#    if a%2==0:
-#     sumo=sumo+a
-#     print("sum of odd numbers",sumo)
-#    else: 
-#     sume=sume+a 
-#     print("sum of even numbers",sume)
-# i+=1    
+#     if i%2==0:
+#      sumeven+=i
+#     else:
+#       sumodd+=i
+#     i+=1  
+# print("sum of odd number:",sumodd)      
+# print("sum of even number:",sumeven) 
 
 
+# multiplication using while
+# --------------------------
+# a=int(input("enter a number:" ))
+# i=0
+# while i<=10:
+#  print(i,"x",a,"=",i*a) 
+#  i=i+1
+
+# print even numbers using while
+# -----------------------------
+# a=int(input("enter a integer:"))
+# i=2
+# while i<=a:
+#    print(i)
+#    i=i+2 
 
 
+# print odd numbers using while
+# -----------------------------
+# a=int(input("enter a integer:"))
+# i=1
+# while i<=a:
+#    print(i)
+#    i=i+2
+
+# list of integers
+# -----------------
+# a=input("Enter a list:")
+a=[1,2,3,4,5,6,7,8,9]
 
 
-
-# # sumeven=0
-# # sumodd=0
-# # a=int(input("enter a number :"))
-# # while a!=0:
-# #     if a%2==0:
-# #      sumeven+=a
-# #      print("sum of even numbers",sumeven)
-# #     else:
-# #       sumodd+=a
-# #       print("sum of even numbers",sumodd)
 
