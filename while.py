03fe1ba9aa9dc0843839dd17815caf3369d41fb1
arya <aryakp841@gmail>
2023-08-16 08:57:22 +0530

    file added

--- while.py
+++ while.py
@@ -42,8 +42,27 @@
 
 # list of integers
 # -----------------
-# a=input("Enter a list:")
-a=[1,2,3,4,5,6,7,8,9]
+# a=[1,2,3,4,5,6,7,8,9,10]
+# i=0
+# while i < len(a):
+#   if a[i]>5.5:
+#    print(a[i])
+#   i+=1
+
+# a="Aryakp"
+# i=0
+# while i<len(a):
+#     print(a[i])
+#     i+=1
 
 
-
+a=str(input("Enter a string:"))
+b=["a","e","i","o","u","A","E","I","O","U"]
+c=0
+i=0
+while i<len(a):
+  if a[i] in b: 
+    # print(a[i])
+   c+=1
+   print(a[i],"=",c)
+  i+=1
