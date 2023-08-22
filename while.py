b263d3b98aa4ddbcb118ee86eca2ee3afe6ee560
arya <aryakp184@gmail.com>
2023-08-22 08:55:43 +0530

    added files

--- while.py
+++ while.py
@@ -81,9 +81,21 @@
 #         print(a[i])
 #         i+=1
         
+#uppercase and lowere case
+#-------------------------
+        
 # a=str(input("Enter a string:"))
+# countU=0
+# countL=0
 # i=0
 # while i<len(a):
-#     if a.isupper:
+#     if a[i].isupper():
+#         countU=countU+1
+#     elif a[i].islower():
+#         countL=countL+1
+   
+#     i+=1        
+# print("no.of uppercase characters",countU)
+# print("no.of lowercase characters",countL)
         
     
